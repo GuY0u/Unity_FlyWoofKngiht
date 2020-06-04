8c13277acbbbd23244c009a1b8593f21393865ab
GuYou <yysh123@gmail.com>
2020-06-04 14:11:57 +0900

    보스 생성
    - 보스 큐브 생성
    - 보스 패턴1 : 플레이어의 위치 방향으로 1초 마다 총알발사
    - 보스 패턴2 : 2초마다 일정각도방향으로 총알을 뿌림

--- Unity Project/Assets/_Gu/Scripts/BossBullet.cs
+++ Unity Project/Assets/_Gu/Scripts/BossBullet.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class BossBullet : MonoBehaviour
 {
     //총알클래스 하는일
     //플레이어가 발사 버튼을 누르면
     //총알이 생성된 후 발사방향으로 움직인다.
 
-
+    public GameObject target;
 
     public float speed = 10.0f;
 
+    Vector3 dir;
+
+    private void Start()
+    {
+        target = GameObject.FindWithTag("Player");
 
 
+        dir = (target.transform.position - transform.position).normalized;
+    }
 
     void Update()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
 
+        transform.Translate(dir * speed * Time.deltaTime);
     }
 
     ////카메라 화면밖으로 나가서 보이지 않게 되면
---
+++ Unity Project/Assets/_Gu/Scripts/BulletManager.cs
@@ -0,0 +1,55 @@
+﻿//using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    //애너미매니저의 역할?
+    //애너미들을 공장에서 찍어낸다(애너미 프리팹)
+
+    //필요한 것
+    //애너미 스폰타임
+    //애너미 스폰위치
+
+    public GameObject here; 
+    
+    public GameObject enemyFactory;     //애너미 공장(애너미프리팹)
+    public GameObject spawnPoint;       //스폰위치
+    public float spawnTime = 1.0f;       //스폰타임(생성주기)
+    public float curTime;                //누적타임
+
+    private void Start()
+    {
+        here = GetComponent<GameObject>();
+    }
+
+    void Update()
+    {
+        SpawnEnemy();
+    }
+
+    private void SpawnEnemy()
+    {
+        //몇초에 한번씩 이벤트 발동
+        //시간 누적타임으로 계산한다
+        //게임에서 정말 자주 사용!
+
+        curTime += Time.deltaTime;
+        if (curTime > spawnTime)
+        {
+            //누적된 현재시간을 0초로 초기화(안하면 curTime의 숫자가 계속 누적됨)
+            curTime = 0.0f;
+
+            //스폰타임 랜덤
+            spawnTime = Random.Range(0.5f, 2.0f);
+
+            //애너미 생성
+            GameObject enemy = Instantiate(enemyFactory);
+            enemy.transform.position = spawnPoint.transform.position;
+            //enemy.transform.position = transform.GetChild(index).position;
+            //enemy.transform.position = spawnPoint[index].transform.position;
+
+        }
+    }
+}
